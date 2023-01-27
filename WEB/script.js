83098c2d7f865fac1e7779868d8c5b3275ed25f0
RojanGamingYT <71162720+RojanGamingYT@users.noreply.github.com>
2023-01-27 14:55:07 +0545

    TESTS

--- WEB/script.js
+++ WEB/script.js
@@ -1,6 +1,19 @@
 const tweetButton = document.querySelector('#tweet-button');
 const tweetInput = document.querySelector('#tweet-input');
 const tweetContainer = document.querySelector('#tweet-container');
+const newTweet = document.createElement('div');
+const likeButton = document.createElement('button');
+const retweetButton = document.createElement('button');
+
+
+likeButton.classList.add('like-button');
+likeButton.innerHTML = 'Like';
+retweetButton.classList.add('retweet-button');
+retweetButton.innerHTML = 'Retweet';
+
+newTweet.appendChild(likeButton);
+newTweet.appendChild(retweetButton);
+
 
 tweetButton.addEventListener('click', function() {
   if(tweetInput.value){
@@ -11,3 +24,4 @@ tweetButton.addEventListener('click', function() {
   tweetInput.value = '';
   }
 });
+
